007bb7b135a212b50263700cac282d55cbb91b04
jungdadak <junghyun21110@gmail.com>
2024-10-28 06:55:58 +0900

    '13'

--- src/page/AdminProductPage/component/NewItemDialog.js
+++ src/page/AdminProductPage/component/NewItemDialog.js
@@ -31,7 +31,6 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
 	const [stock, setStock] = useState([]);
 	const dispatch = useDispatch();
 	const [stockError, setStockError] = useState(false);
-	console.log(stock, "stock");
 
 	useEffect(() => {
 		if (success) setShowDialog(false);
@@ -65,7 +64,16 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
 	const handleSubmit = (event) => {
 		event.preventDefault();
 		//재고를 입력했는지 확인, 아니면 에러
+		console.log(stock, "stock");
+		console.log(formData, "formData");
+		if (stock.length === 0) {
+			setStockError(true);
+			return;
+		}
 		// 재고를 배열에서 객체로 바꿔주기
+		const totalStock = stock.reduce((total, item) => {
+			return { ...total, [item[0]]: parseInt(item[1]) };
+		}, {});
 		// [['M',2]] 에서 {M:2}로
 		if (mode === "new") {
 			//새 상품 만들기
@@ -106,6 +114,7 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
 	};
 
 	const onHandleCategory = (event) => {
+		//카테고리가 추가되어 있으면 제거
 		if (formData.category.includes(event.target.value)) {
 			const newCategory = formData.category.filter(
 				(item) => item !== event.target.value
@@ -115,6 +124,7 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
 				category: [...newCategory],
 			});
 		} else {
+			// 아니면 새로 추가
 			setFormData({
 				...formData,
 				category: [...formData.category, event.target.value],
@@ -124,6 +134,7 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
 
 	const uploadImage = (url) => {
 		//이미지 업로드
+		setFormData({ ...formData, image: url });
 	};
 
 	return (
