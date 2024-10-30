0ed4be9e28cd3e3fe6e65883b4733105c6342334
jungdadak <junghyun21110@gmail.com>
2024-10-31 00:36:41 +0900

    'ddd'

--- src/features/product/productSlice.js
+++ src/features/product/productSlice.js
@@ -8,11 +8,11 @@ export const getProductList = createAsyncThunk(
 	async (query, { rejectWithValue }) => {
 		try {
 			const response = await api.get("/product");
-			if (response.status !== 200) throw new Error(response.error);
-
+			if (response.status !== 200 && response.status !== 201)
+				throw new Error(response.error);
 			return response.data.products;
 		} catch (error) {
-			rejectWithValue(error.message);
+			rejectWithValue(error.error);
 		}
 	}
 );
@@ -24,7 +24,19 @@ export const getProductDetail = createAsyncThunk(
 
 export const createProduct = createAsyncThunk(
 	"products/createProduct",
-	async (formData, { dispatch, rejectWithValue }) => {}
+	async (formData, { dispatch, rejectWithValue }) => {
+		try {
+			const response = await api.post("/product", formData);
+			if (response.status !== 200 && response.status !== 201)
+				throw new Error(response.error);
+			dispatch(
+				showToastMessage({ message: "상품이 등록되었습니다.", status: "success" })
+			);
+			return response.data.data;
+		} catch (error) {
+			return rejectWithValue(error.error);
+		}
+	}
 );
 
 export const deleteProduct = createAsyncThunk(
@@ -62,24 +74,23 @@ const productSlice = createSlice({
 	},
 	extraReducers: (builder) => {
 		builder
-			.addCase(createProduct.pending, (state) => {
+			.addCase(createProduct.pending, (state, action) => {
 				state.loading = true;
 			})
-			.addCase(createProduct.fulfilled, (state) => {
+			.addCase(createProduct.fulfilled, (state, action) => {
 				state.loading = false;
-				state.success = true;
 				state.error = "";
+				state.success = true; // 상품생성 성공시 다이얼로그 닫고, 실패하면 메시지 띄우기 &닫지않음
 			})
 			.addCase(createProduct.rejected, (state, action) => {
 				state.loading = false;
 				state.error = action.payload;
 				state.success = false;
 			})
-			.addCase(getProductList.pending, (state) => {
+			.addCase(getProductList.pending, (state, action) => {
 				state.loading = true;
 			})
 			.addCase(getProductList.fulfilled, (state, action) => {
-				console.log("action.payload:", action.payload);
 				state.loading = false;
 				state.productList = action.payload;
 				state.error = "";
